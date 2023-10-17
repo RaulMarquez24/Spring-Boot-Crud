9f89bb87b9af65f04e9a0d0e7621e9d6e0d2229a
Raúl Márquez <raulmu785@gmail.com>
2023-10-17 11:46:37 +0200

    FacturaPdfView

---
+++ src/main/java/com/bolsadeideas/springboot/app/view/pdf/FacturaPdfView.java
@@ -0,0 +1,43 @@
+package com.bolsadeideas.springboot.app.view.pdf;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractPdfView;
+
+import com.bolsadeideas.springboot.app.models.entity.Factura;
+import com.lowagie.text.Document;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component("factura/ver")
+public class FacturaPdfView extends AbstractPdfView {
+
+    @Override
+    protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
+                
+        Factura factura = (Factura) model.get("factura");
+
+        PdfPTable tabla1 = new PdfPTable(1);
+        tabla1.addCell("Datos del Cliente");
+        tabla1.addCell(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
+        tabla1.addCell(factura.getCliente().getEmail());
+
+        PdfPTable tabla2 = new PdfPTable(1);
+        tabla2.addCell("Datos de la Factura");
+        tabla2.addCell("Folio: " + factura.getId());
+        tabla2.addCell("Descripción: " + factura.getDescripcion());
+        tabla2.addCell("Fecha: " + factura.getCreateAt());
+
+        document.add(tabla1);
+        document.add(tabla2);
+
+        
+
+    }
+    
+}
